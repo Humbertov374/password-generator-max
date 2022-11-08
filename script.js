ddd6b6331dc88dbe0799377fc0eed6d15da031bd
Humbertov374 <humbertov374@gmail.com>
2022-11-08 16:32:47 -0700

    Added if's stating that if user does not meet conditions they will be returned to start

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ while (true) {
     }
   )
 
+  if (passwordLengthResult.canceled) return // exited prompt
+    
+      // if input type is invalid (not a number)
+      if (!passwordLengthResult.isValidType) {
+        window.alert("Please enter a valid number")
+    
+      // if input type is valid but does not meet the condition (password length range)
+      } else if (!passwordLengthResult.isValidCondition) {
+        window.alert("Password length must be between " + minLength + " and " + maxLength + " characters")
+    
+      // else no other invalidations occur, break the prompt loop
+      } else {
+        break
+      }
+    }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
